2afdcdefccf2f219f322a0b94c5bc30816c9f4a3
-m <Tymon.Oziemblewski@gmail.com>
2019-01-16 03:06:37 +0100

    16160

--- js/scrapper.js
+++ js/scrapper.js
@@ -1,34 +1,34 @@
-const rp = require('request-promise');
+const rp = require("request-promise");
 const jsdom = require("jsdom");
-const {
-    JSDOM
-} = jsdom;
+const { JSDOM } = jsdom;
 
-const baseUrl = 'https://lichess.org/training/';
+const baseUrl = "https://lichess.org/training/";
 
 async function logFen(puzzleID) {
     let result = await rp(baseUrl + puzzleID)
-        .then(function (html) {
+        .then(function(html) {
             const dom = new JSDOM(html, {
                 runScripts: "dangerously",
                 pretendToBeVisual: true
             });
-            var puzzle = dom.window.lichess.puzzle.data
-            var length = puzzle.puzzle.initialPly
+            var puzzle = dom.window.lichess.puzzle.data;
+            var length = puzzle.puzzle.initialPly;
             var fen = puzzle.game.treeParts[length].fen;
             console.log(fen);
         })
-        .catch(function (err) {
+        .catch(function(err) {
             console.log(err);
         });
     return result;
 }
 
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise(resolve => {
+        return setTimeout(resolve, ms);
+    });
 }
 
-var iterator = 1531;
+var iterator = 1673;
 async function callSequence() {
     while (true) {
         await logFen(iterator++);
